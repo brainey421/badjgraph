04eafe6bea705774ec9c067e63902459cfb9f62f
Bryan Rainey <brainey421@gmail.com>
2016-03-29 20:11:31 +0000

    Include transpose file.

---
+++ transpose.c
@@ -0,0 +1,31 @@
+#include "graph.h"
+
+/* Transposes a BADJ graph. */
+int main(int argc, char *argv[])
+{
+    // Check arguments
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: ./transpose [BADJ file] [transposed BADJ file]\n");
+        return 1;
+    }
+    
+    // Initialize graph
+    graph g;
+    if (initialize(&g, argv[1], 1))
+    {
+        return 1;
+    }
+
+    // Print numbers of nodes and edges
+    fprintf(stderr, "Nodes: %llu\n", g.n);
+    fprintf(stderr, "Edges: %llu\n\n", g.m);
+
+    // Transpose graph
+    transpose(&g, argv[2]);
+
+    // Destroy graph
+    destroy(&g);
+
+    return 0;
+}
